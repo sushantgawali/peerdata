9f8073b1ff6679dd4a4a85dc800536257bdbda84
sushant gawali <sushant.gawali@live.com>
2014-09-20 12:22:13 +0530

    initial setup added

---
+++ main.js
@@ -0,0 +1,300 @@
+'use strict';
+
+var sendChannel;
+var sendButton = document.getElementById("sendButton");
+var sendTextarea = document.getElementById("dataChannelSend");
+var receiveTextarea = document.getElementById("dataChannelReceive");
+
+sendButton.onclick = sendData;
+
+var isChannelReady;
+var isInitiator;
+var isStarted;
+var localStream;
+var pc;
+var remoteStream;
+var turnReady;
+
+var pc_config = webrtcDetectedBrowser === 'firefox' ?
+{'iceServers':[{'url':'stun:23.21.150.121'}]} : // number IP
+{'iceServers': [{'url': 'stun:stun.l.google.com:19302'}]};
+
+var pc_constraints = {
+    'optional': [
+        {'DtlsSrtpKeyAgreement': true},
+        {'RtpDataChannels': true}
+    ]};
+
+// Set up audio and video regardless of what devices are present.
+var sdpConstraints = {'mandatory': {
+    'OfferToReceiveAudio':true,
+    'OfferToReceiveVideo':true }};
+
+/////////////////////////////////////////////
+
+var room = location.pathname.substring(1);
+if (room === '') {
+//  room = prompt('Enter room name:');
+    room = 'foo';
+} else {
+    //
+}
+
+var socket = io.connect();
+
+if (room !== '') {
+    console.log('Create or join room', room);
+    socket.emit('create or join', room);
+}
+
+socket.on('created', function (room){
+    console.log('Created room ' + room);
+    isInitiator = true;
+});
+
+socket.on('full', function (room){
+    console.log('Room ' + room + ' is full');
+});
+
+socket.on('join', function (room){
+    console.log('Another peer made a request to join room ' + room);
+    console.log('This peer is the initiator of room ' + room + '!');
+    isChannelReady = true;
+});
+
+socket.on('joined', function (room){
+    console.log('This peer has joined room ' + room);
+    isChannelReady = true;
+});
+
+socket.on('log', function (array){
+    console.log.apply(console, array);
+});
+function sendMessage(message){
+    console.log('Sending message: ', message);
+    socket.emit('message', message);
+}
+
+socket.on('message', function (message){
+    console.log('Received message:', message);
+    if (message === 'got user media') {
+        maybeStart();
+    } else if (message.type === 'offer') {
+        if (!isInitiator && !isStarted) {
+            maybeStart();
+        }
+        pc.setRemoteDescription(new RTCSessionDescription(message));
+        doAnswer();
+    } else if (message.type === 'answer' && isStarted) {
+        pc.setRemoteDescription(new RTCSessionDescription(message));
+    } else if (message.type === 'candidate' && isStarted) {
+        var candidate = new RTCIceCandidate({sdpMLineIndex:message.label,
+            candidate:message.candidate});
+        pc.addIceCandidate(candidate);
+    } else if (message === 'bye' && isStarted) {
+        handleRemoteHangup();
+    }
+});
+
+////////////////////////////////////////////////////
+
+
+if (location.hostname != "localhost") {
+    requestTurn('https://computeengineondemand.appspot.com/turn?username=41784574&key=4080218913');
+}
+
+function maybeStart() {
+    if (!isStarted && localStream && isChannelReady) {
+        createPeerConnection();
+        pc.addStream(localStream);
+        isStarted = true;
+        if (isInitiator) {
+            doCall();
+        }
+    }
+}
+
+window.onbeforeunload = function(e){
+    sendMessage('bye');
+}
+
+/////////////////////////////////////////////////////////
+
+function createPeerConnection() {
+    try {
+        pc = new RTCPeerConnection(pc_config, pc_constraints);
+        pc.onicecandidate = handleIceCandidate;
+        console.log('Created RTCPeerConnnection with:\n' +
+            '  config: \'' + JSON.stringify(pc_config) + '\';\n' +
+            '  constraints: \'' + JSON.stringify(pc_constraints) + '\'.');
+    } catch (e) {
+        console.log('Failed to create PeerConnection, exception: ' + e.message);
+        alert('Cannot create RTCPeerConnection object.');
+        return;
+    }
+    pc.onaddstream = handleRemoteStreamAdded;
+    pc.onremovestream = handleRemoteStreamRemoved;
+
+    if (isInitiator) {
+        try {
+            // Reliable Data Channels not yet supported in Chrome
+            sendChannel = pc.createDataChannel("sendDataChannel",
+                {reliable: false});
+            sendChannel.onmessage = handleMessage;
+            trace('Created send data channel');
+        } catch (e) {
+            alert('Failed to create data channel. ' +
+                'You need Chrome M25 or later with RtpDataChannel enabled');
+            trace('createDataChannel() failed with exception: ' + e.message);
+        }
+        sendChannel.onopen = handleSendChannelStateChange;
+        sendChannel.onclose = handleSendChannelStateChange;
+    } else {
+        pc.ondatachannel = gotReceiveChannel;
+    }
+}
+
+function sendData() {
+    var data = sendTextarea.value;
+    sendChannel.send(data);
+    trace('Sent data: ' + data);
+}
+
+function gotReceiveChannel(event) {
+    trace('Receive Channel Callback');
+    sendChannel = event.channel;
+    sendChannel.onmessage = handleMessage;
+    sendChannel.onopen = handleReceiveChannelStateChange;
+    sendChannel.onclose = handleReceiveChannelStateChange;
+}
+
+function handleMessage(event) {
+    trace('Received message: ' + event.data);
+    receiveTextarea.value = event.data;
+}
+
+function handleSendChannelStateChange() {
+    var readyState = sendChannel.readyState;
+    trace('Send channel state is: ' + readyState);
+    enableMessageInterface(readyState == "open");
+}
+
+function handleReceiveChannelStateChange() {
+    var readyState = sendChannel.readyState;
+    trace('Receive channel state is: ' + readyState);
+    enableMessageInterface(readyState == "open");
+}
+
+function enableMessageInterface(shouldEnable) {
+    if (shouldEnable) {
+        dataChannelSend.disabled = false;
+        dataChannelSend.focus();
+        dataChannelSend.placeholder = "";
+        sendButton.disabled = false;
+    } else {
+        dataChannelSend.disabled = true;
+        sendButton.disabled = true;
+    }
+}
+
+function handleIceCandidate(event) {
+    console.log('handleIceCandidate event: ', event);
+    if (event.candidate) {
+        sendMessage({
+            type: 'candidate',
+            label: event.candidate.sdpMLineIndex,
+            id: event.candidate.sdpMid,
+            candidate: event.candidate.candidate});
+    } else {
+        console.log('End of candidates.');
+    }
+}
+
+function doCall() {
+    var constraints = {'optional': [], 'mandatory': {'MozDontOfferDataChannel': true}};
+    // temporary measure to remove Moz* constraints in Chrome
+    if (webrtcDetectedBrowser === 'chrome') {
+        for (var prop in constraints.mandatory) {
+            if (prop.indexOf('Moz') !== -1) {
+                delete constraints.mandatory[prop];
+            }
+        }
+    }
+    constraints = mergeConstraints(constraints, sdpConstraints);
+    console.log('Sending offer to peer, with constraints: \n' +
+        '  \'' + JSON.stringify(constraints) + '\'.');
+    pc.createOffer(setLocalAndSendMessage, null, constraints);
+}
+
+function doAnswer() {
+    console.log('Sending answer to peer.');
+    pc.createAnswer(setLocalAndSendMessage, null, sdpConstraints);
+}
+
+function mergeConstraints(cons1, cons2) {
+    var merged = cons1;
+    for (var name in cons2.mandatory) {
+        merged.mandatory[name] = cons2.mandatory[name];
+    }
+    merged.optional.concat(cons2.optional);
+    return merged;
+}
+
+
+function requestTurn(turn_url) {
+    var turnExists = false;
+    for (var i in pc_config.iceServers) {
+        if (pc_config.iceServers[i].url.substr(0, 5) === 'turn:') {
+            turnExists = true;
+            turnReady = true;
+            break;
+        }
+    }
+    if (!turnExists) {
+        console.log('Getting TURN server from ', turn_url);
+        // No TURN server. Get one from computeengineondemand.appspot.com:
+        var xhr = new XMLHttpRequest();
+        xhr.onreadystatechange = function(){
+            if (xhr.readyState === 4 && xhr.status === 200) {
+                var turnServer = JSON.parse(xhr.responseText);
+                console.log('Got TURN server: ', turnServer);
+                pc_config.iceServers.push({
+                    'url': 'turn:' + turnServer.username + '@' + turnServer.turn,
+                    'credential': turnServer.password
+                });
+                turnReady = true;
+            }
+        };
+        xhr.open('GET', turn_url, true);
+        xhr.send();
+    }
+}
+
+
+
+function extractSdp(sdpLine, pattern) {
+    var result = sdpLine.match(pattern);
+    return result && result.length === 2 ? result[1] : null;
+}
+
+// Strip CN from sdp before CN constraints is ready.
+function removeCN(sdpLines, mLineIndex) {
+    var mLineElements = sdpLines[mLineIndex].split(' ');
+    // Scan from end for the convenience of removing an item.
+    for (var i = sdpLines.length-1; i >= 0; i--) {
+        var payload = extractSdp(sdpLines[i], /a=rtpmap:(\d+) CN\/\d+/i);
+        if (payload) {
+            var cnPos = mLineElements.indexOf(payload);
+            if (cnPos !== -1) {
+                // Remove CN payload from m line.
+                mLineElements.splice(cnPos, 1);
+            }
+            // Remove CN line in sdp
+            sdpLines.splice(i, 1);
+        }
+    }
+
+    sdpLines[mLineIndex] = mLineElements.join(' ');
+    return sdpLines;
+}
+
